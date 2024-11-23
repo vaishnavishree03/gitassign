502cd3526b7a6305e50c9ddb65e9db87efb6cc42
vaishnavishree03 <vaishnaveeshree03@gmail.com>
2024-11-24 00:47:02 +0530

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,16 @@
-FROM openjdk:17
-COPY target/assignmenttesting-1.0.jar /assignmenttesting.jar
-ENTRYPOINT ["sh", "-c", "java -jar /assignmenttesting.jar && sleep infinity"]
+FROM openjdk:17-jdk-alpine
+RUN apk --no-cache add \
+maven \
+chromium \
+chromium-chromedriver \
+bash \
+&& rm -rf /var/cache/apk/*
+
+ENV CHROME_BIN=/usr/bin/chromium-browser
+ENV CHROME_DRIVER=/usr/bin/chromiumdriver
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+CMD ["mvn","clean","test"]
--- src/test/java/com/assignmenttesting/AssignmentTesting.java
+++ src/test/java/com/assignmenttesting/AssignmentTesting.java
@@ -1,40 +1,40 @@
-package com.assignmenttesting;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-public class AssignmentTesting {
-	public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
- try {
-             driver.get("http://127.0.0.1:5500/signin.html"); 
-             driver.manage().window().maximize();
-
-            WebElement nameInput = driver.findElement(By.cssSelector("#name"));
-            WebElement radioMale = driver.findElement(By.cssSelector("#gender-male"));
-            WebElement radioFemale = driver.findElement(By.cssSelector("#gender-female"));
-            WebElement roleDropdown = driver.findElement(By.cssSelector("#role"));
-            WebElement submitButton = driver.findElement(By.cssSelector("#submit-btn"));
-
-            nameInput.sendKeys("vaishnavi"); 
-            radioMale.click(); 
-            Select selectRole = new Select(roleDropdown);
-            selectRole.selectByValue("tester"); 
-            Thread.sleep(1000); 
-            submitButton.click(); 
-
-            System.out.println("Form submitted successfully!");
-        } catch (Exception e) {
-            e.printStackTrace(); 
-        } finally {
-            
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            driver.quit();
-        }
-    }
-}
+//package com.assignmenttesting;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.Select;
+//public class AssignmentTesting {
+//	public static void main(String[] args) {
+//        WebDriver driver = new ChromeDriver();
+// try {
+//             driver.get("http://127.0.0.1:5500/signin.html"); 
+//             driver.manage().window().maximize();
+//
+//            WebElement nameInput = driver.findElement(By.cssSelector("#name"));
+//            WebElement radioMale = driver.findElement(By.cssSelector("#gender-male"));
+//            WebElement radioFemale = driver.findElement(By.cssSelector("#gender-female"));
+//            WebElement roleDropdown = driver.findElement(By.cssSelector("#role"));
+//            WebElement submitButton = driver.findElement(By.cssSelector("#submit-btn"));
+//
+//            nameInput.sendKeys("vaishnavi"); 
+//            radioMale.click(); 
+//            Select selectRole = new Select(roleDropdown);
+//            selectRole.selectByValue("tester"); 
+//            Thread.sleep(1000); 
+//            submitButton.click(); 
+//
+//            System.out.println("Form submitted successfully!");
+//        } catch (Exception e) {
+//            e.printStackTrace(); 
+//        } finally {
+//            
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            driver.quit();
+//        }
+//    }
+//}
